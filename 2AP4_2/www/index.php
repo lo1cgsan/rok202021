d95efa97192f65522f7c2e011db43870868f2d2b
lo1cgsan <lo1cgsan@gmail.com>
2021-06-15 14:22:02 +0200

    html 1A i PHP 2AP4_2

---
+++ 2AP4_2/www/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Pierwszy skrypt PHP</title>
+    <style>
+        body {
+            font-family: Arial,sans-serif;
+            font-size: 16px;
+            background-color: #F7F222;
+        }
+    </style>
+</head>
+
+<body>
+    
+    <?php
+        $a = 5;
+        $b = 2;
+        echo "<h1>Podstawy PHP</h1>";
+        echo "<p>Jakiś przykładowy tekst</p>";
+        echo "<p>Suma a i b: ".($a + $b)."</p>";
+    ?>
+
+</body>
+</html>
