bd4ad9561b3e7215fe24191b9e4ac9b9117b6954
lo1cgsan <lo1cgsan@gmail.com>
2021-06-17 14:21:36 +0200

    php_1 2AP4_1

---
+++ 2AP4_1/www/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Podstawy skryptów PHP</title>
+    <style>
+        body {
+            font-family: Arial,sans-serif;
+            font-size: 16px;
+        }
+        h1 {
+           font-size: 2rem; 
+        }
+        p {
+            font-size: 1.2rem;
+        }
+    </style>
+</head>
+
+<body>
+
+<?php
+
+    $a = 10;
+    $b = 30;
+
+    echo "<h1>Podstawy skryptów PHP</h1>";
+    echo "<p>Treść dokumentu</p>";
+    echo "<p>Suma a i b: ".($a + $b)."</p>";
+
+?>
+
+</body>
+
+</html>
